3661e57b4a8e396dc5d1525fc0e2411edbd3fe49
Kristaps <53013669+siltais@users.noreply.github.com>
2024-03-04 14:59:49 +0200

    3.4 exercise

--- index.js
+++ index.js
@@ -44,7 +44,13 @@ app.get('/api/persons/:id', (request, response) => {
     } else {
       response.status(404).end()
     }
-  })
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id) 
+    response.status(204).end()
+})
   
 const PORT = 3001
 app.listen(PORT, () => {

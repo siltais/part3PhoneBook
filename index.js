0e641a7f24f07fc5ffabc7839cd41512fb49e809
Kristaps <53013669+siltais@users.noreply.github.com>
2024-03-04 17:48:09 +0200

    3.8 exercise

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const app = express()
 const morgan = require('morgan')
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :dataSent'))
+morgan.token('dataSent', function(req) {
+    if(req.method === 'POST'){
+        return JSON.stringify(req.body)
+    }
+})
+
 
 let persons = [
     { 

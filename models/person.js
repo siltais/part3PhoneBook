1a95a6c54b75c01f2344d57167772a32a87ce446
Kristaps <53013669+siltais@users.noreply.github.com>
2024-03-07 10:34:31 +0200

    3.20

--- models/person.js
+++ models/person.js
@@ -20,7 +20,17 @@ mongoose.connect(url)
         minLength: 3,
         required: true
     },
-    number: String,
+    number: {
+      type: String,
+      minLength: 8,
+      validate: {
+        validator: function(v) {
+          return /^\d{2,}\-\d{3,}$/.test(v);
+        },
+        message: props => `${props.value} is not a valid phone number!`
+      },
+      required: true
+    }
 
   })
   
